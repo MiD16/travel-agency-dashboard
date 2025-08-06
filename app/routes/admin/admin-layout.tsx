9a829ea6d957135cf15a989fee60408209ed8769
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-08-06 15:43:32 +0300

    Navbar - Checkpoint 1

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Outlet } from 'react-router'
+import React from "react";
+import { Outlet } from "react-router";
+import { SidebarComponent } from '@syncfusion/ej2-react-navigations'
+import NavItems from "components/NavItems";
 
 const AdminLayout = () => {
   return (
-    <div className='admin-layout'>
-        AdminLayout
-        <aside className='w-full max-w-[270px] hidden lg:block'>Sidebar</aside>
-        <aside className='children'>
-            <Outlet />
-        </aside>
+    <div className="admin-layout">
+      AdminLayout
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
+      <aside className="children">
+        <Outlet />
+      </aside>
     </div>
-  )
-}
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;

02254b07f2ea216515ebfcdc1bf7c4557836c1a9
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-08-09 11:28:21 +0300

    Trip Form

---
+++ app/routes/admin/trips.tsx
@@ -0,0 +1,17 @@
+import Header from 'components/Header'
+import React from 'react'
+
+const Trips = () => {
+  return (
+    <main className="all-users wrapper">
+      <Header
+        title="Trips"
+        description="View and Edit AI-Generated travel plans"
+        ctaText='Create a trip'
+        ctaUrl='/trips/create'
+      />
+      </main>
+  )
+}
+
+export default Trips
